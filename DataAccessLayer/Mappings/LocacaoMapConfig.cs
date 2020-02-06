bafeac0558af20f622ee4a8f677bb6e4c3dc003a
Thiago Morlo <thiagomorlo14@gmail.com>
2020-02-06 17:18:01 -0300

    DAO

---
+++ DataAccessLayer/Mappings/LocacaoMapConfig.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Mappings
+{
+    internal class LocacaoMapConfig : EntityTypeConfiguration<Locacao>
+    {
+        public LocacaoMapConfig()
+        {
+            this.ToTable("LOCACOES");
+            this.Property(l => l.DataDevolucao).HasColumnType("date");
+            this.Property(l => l.DataLocacao).HasColumnType("date").IsRequired();
+            this.Property(l => l.DataPrevistaDevolucao).HasColumnType("date");
+            this.Property(l => l.Preco).IsRequired();
+
+        }
+    }
+}
